cfdb5d7d4e3bacccbae91333890af3eba3ae7ebe
AHAEIVA <aha@eiva.com>
2025-04-18 16:26:03 +0200

    Fix import error in train_model.py

--- training/train_model.py
+++ training/train_model.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import os
 import sys # Keep sys import if needed elsewhere, otherwise remove
 import time
